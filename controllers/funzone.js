91e5b05f7909b6611f2a9110b48ae9c0a3e84627
NalatiRavali <89615971+NalatiRavali@users.noreply.github.com>
2021-11-12 14:35:19 -0600

    modified

--- controllers/funzone.js
+++ controllers/funzone.js
@@ -1,3 +1,4 @@
+const funzone = require('../models/funzone');
 var Costume = require('../models/funzone'); 
  
 // List of all Costumes 
@@ -55,7 +56,7 @@ exports.costume_update_put = async function(req, res) {
     console.log(`update on id ${req.params.id} with body 
 ${JSON.stringify(req.body)}`) 
     try { 
-        let toUpdate = await Costume.findById( req.params.id) 
+        let toUpdate = await funzone.findById( req.params.id) 
         // Do updates of properties 
         if(req.body.costume_type)  
                toUpdate.costume_type = req.body.costume_type; 
